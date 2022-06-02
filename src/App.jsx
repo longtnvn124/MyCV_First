b63acc680d4e9adf713617c46dead2baccfba58e
longtnvn124 <longkakainfo@gmail.com>
2022-06-03 01:03:10 +0700

    update file app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
-import Experience from "./components/Expenrience/Experience";
+import Experience from "./components/experience/Experience";
 import Services from './components/services/Services'
 import Prortfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
